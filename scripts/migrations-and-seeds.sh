bb133b0666b749bc26784526b689f9a1071554e7
Andy Roddam <andybendyman@hotmail.com>
2014-11-24 11:57:12 +0000

    Create migrations-and-seeds.sh

---
+++ scripts/migrations-and-seeds.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+MAP=$1
+
+#run migrations
+echo "Running migrations for $MAP";
+cd /home/vagrant/projects/$MAP;
+php artisan migrate;
+
+#run migrations
+echo "Running seeds for $MAP";
+cd /home/vagrant/projects/$MAP;
+php artisan db:seed;
